c9691b8453c7488c26d482bf3e1e1fd72e764cf8
kevin <kevin.chiu@deledao.com>
2023-02-17 14:03:57 +0800

    ** Lifing state up & lifing click() up onto Board

--- src/App.js
+++ src/App.js
@@ -3,14 +3,9 @@ import React from "react";
 
 
 
-function Square() {
-  const [value,setValue] = useState(null);
+function Square({value,onSquareClick}) {
+  // const [value,setValue] = useState(null);
   
-  function onSquareClick(){
-    console.log('click');
-    setValue('X');
-  }
-
   return (
     <button className="square" onClick={onSquareClick}>
       {value}
@@ -19,22 +14,31 @@ function Square() {
 }
 
 export default function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+
+  console.log(squares);
+
+  function handleClick() {
+    console.log('handle Click func');
+  }
+
+
   return (
     <div className="game-board">
-      <div className='board-row'>
-        <Square />
-        <Square />
-        <Square />
+      <div className="board-row">
+        <Square value={squares[0]} onSquareClick={handleClick}/>
+        <Square value={squares[1]} onSquareClick={handleClick}/>
+        <Square value={squares[2]} onSquareClick={handleClick}/>
       </div>
-      <div className='board-row'>
-        <Square />
-        <Square />
-        <Square />
+      <div className="board-row">
+        <Square value={squares[3]} />
+        <Square value={squares[4]} />
+        <Square value={squares[5]} />
       </div>
-      <div className='board-row'>
-        <Square />
-        <Square />
-        <Square />
+      <div className="board-row">
+        <Square value={squares[6]} />
+        <Square value={squares[7]} />
+        <Square value={squares[8]} />
       </div>
     </div>  
   );
